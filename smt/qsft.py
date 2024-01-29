c926afa42f3185aa8b84c93b9e6d12657a2a53f4
Justin Kang <justin.kang@alumni.ubc.ca>
2024-01-29 12:50:29 -0800

    Updating Singleton Detection code to also check for decode success, which resolves bugs in the code

--- smt/qsft.py
+++ smt/qsft.py
@@ -160,7 +160,7 @@ class QSFT:
             for i, (U, M, D) in enumerate(zip(Us, Ms, Ds)):
                 for j, col in enumerate(U.T):
                     if np.linalg.norm(col) ** 2 > cutoff * len(col):
-                        k = singleton_detection(
+                        k, decode_success = singleton_detection(
                             col,
                             method_channel=self.reconstruct_method_channel,
                             method_source=self.reconstruct_method_source,
@@ -169,12 +169,16 @@ class QSFT:
                             nso_subtype="nso1",
                             source_decoder=decoders[i]
                         )
-                        signature = 1 - ((D @ k) > 0)
-                        rho = np.dot(signature, col) / sum(signature)
-                        residual = col - rho * signature
-                        j_qary = dec_to_bin_vec([j], b).T[0]
-                        bin_matching = np.all(((M.T @ k) > 0) == j_qary)
-
+                        if decode_success:
+                            signature = 1 - ((D @ k) > 0)
+                            rho = np.dot(signature, col) / sum(signature)
+                            residual = col - rho * signature
+                            j_qary = dec_to_bin_vec([j], b).T[0]
+                            bin_matching = np.all(((M.T @ k) > 0) == j_qary)
+                        else:
+                            residual = float('inf')
+                            rho = float('inf')
+                            bin_matching = False
                         if verbosity >= 5:
                             print((i, j), np.linalg.norm(residual) ** 2, cutoff * len(col))
                         if (not bin_matching) or np.linalg.norm(residual) ** 2 > cutoff * len(col):
