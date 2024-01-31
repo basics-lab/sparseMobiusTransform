5e3d2605c947b92d14556e0bd14c72c969ebd592
Yigit Efe Erginbas <erginbas@berkeley.edu>
2024-01-30 20:18:29 -0800

    savio experiments nmse vs snr

--- synt_exp/main.sh
+++ synt_exp/main.sh
@@ -1,3 +1,3 @@
 module load python
 source activate sparse-fft
-sbatch synt_exp.sh
+sbatch nmse_vs_snr.sh
---
+++ synt_exp/nmse_vs_snr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=synt_exp
+#SBATCH --account=fc_basics
+#SBATCH --partition=savio2
+#SBATCH --time=12:00:00
+#SBATCH --output=slurm_outputs/%j.out
+#SBATCH --error=slurm_outputs/%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=erginbas@berkeley.edu
+
+python run-tests-nmse-vs-snr.py --jobid=$SLURM_JOB_ID \
+--num_subsample 3 --num_repeat 1 --b 10 --t 6 8 10 12 --n 500 --sparsity 500 --iters 1
